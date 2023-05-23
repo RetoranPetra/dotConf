526c14b06d7e4f729c15636611e17ec471d57709
RetoranPetra <flyro@live.co.uk>
2023-05-23 22:06:12 +0100

    Changed fontconfig once more, updated environment variables with some additions.

--- home/retoran/.env/wayland.sh
+++ home/retoran/.env/wayland.sh
@@ -9,6 +9,7 @@ export QT_QPA_PLATFORMTHEME=qt5ct
 #SDL
 #export SDL_VIDEODRIVER="wayland,x11"
 export SDL_VIDEODRIVER=wayland
+export GDK_BACKEND=wayland
 #XDG
 export XDG_SESSION_TYPE=wayland
 
@@ -18,3 +19,4 @@ export GBM_BACKEND=nvidia-drm
 export __GLX_VENDOR_LIBRARY_NAME=nvidia
 export WLR_NO_HARDWARE_CURSORS=1
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export __GL_GSYNC_ALLOWED=1
