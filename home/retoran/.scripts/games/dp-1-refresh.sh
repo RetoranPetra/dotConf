41a5d899e12e65754f73a4f6a179aec5e208aa2f
RetoranPetra <flyro@live.co.uk>
2023-06-15 18:10:00 +0100

    Reorganised files and undid sway config changes.

---
+++ home/retoran/.scripts/games/dp-1-refresh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+hypr="$(bash ~/.scripts/isHypr.sh)"
+refresh=$1
+if [[ $hypr == 1 ]];then
+  hyprctl "keyword monitor DP-1,2560x1440@$refresh,0x0,1"
+else
+  #TODO: Add sway command that does this.
+  echo "done!"
+fi
---
+++ home/retoran/.scripts/isHypr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ -v HYPRLAND_INSTANCE_SIGNATURE ];then
+  echo "1"
+else
+  echo "0"
+fi
