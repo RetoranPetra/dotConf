4abc9d30ae5c847868c9cb49705568150076333e
RetoranPetra <flyro@live.co.uk>
2023-10-19 12:50:33 +0100

    Added XDG desktop variables for hyprland, enabled smart dwindle drag. Window rule for waydroid.

--- home/retoran/.scripts/hypr/hyprLaunch.zsh
+++ home/retoran/.scripts/hypr/hyprLaunch.zsh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 source "$HOME/.env/wayland.zsh"
 export WLR_DRM_DEVICES="/dev/dri/card1"
+export XDG_SESSION_DESKTOP="Hyprland"
+export XDG_SESSION_TYPE="wayland"
+export XDG_CURRENT_DESKTOP="Hyprland"
 exec Hyprland
