f276832f19ebfa663c6b10a4f0b0d996dfe72438
RetoranPetra <flyro@live.co.uk>
2023-10-31 13:34:22 +0000

    told mako to display on DP-1

--- home/retoran/.scripts/hypr/hyprStart.zsh
+++ home/retoran/.scripts/hypr/hyprStart.zsh
@@ -3,7 +3,7 @@ killall -e xdg-desktop-portal-wlr
 waybar -c "/home/retoran/.config/hypr/waybar/config" &
 hyprpaper &
 wl-paste -t text --watch clipman store --no-persist &
-mako &
+mako --output DP-1&
 source ~/.scripts/myDex.zsh &
 thunar --daemon &
 source "/home/retoran/.scripts/aerox9.zsh" &
