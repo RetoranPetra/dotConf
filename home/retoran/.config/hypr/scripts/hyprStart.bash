6d852b0a98edf67ee051c94996741a5ec6059f0e
RetoranPetra <flyro@live.co.uk>
2024-04-07 15:34:58 +0100

    fix: hyprland waybar launch correctly

--- home/retoran/.config/hypr/scripts/hyprStart.bash
+++ home/retoran/.config/hypr/scripts/hyprStart.bash
@@ -6,11 +6,10 @@ hyprexec() {
 # https://klotzandrew.com/blog/parallel-bash-with-wait
 
 # Tell systemd we're a graphical session so that related services run.
-hyprexec killall -e xdg-desktop-portal-wlr
+killall -e xdg-desktop-portal-wlr
 hyprexec source systemctl --user start hyprland-session.target
 
 # Start up our wanted programs
-hyprexec "waybar -c ~/.config/hypr/waybar/config"
 hyprexec "hyprpaper"
 hyprexec "wl-paste -t text --watch clipman store --no-persist"
 hyprexec "mako --output DP-1"
