a33eb545e0ebc76167c2dda90f39764cf8d3d042
RetoranPetra <flyro@live.co.uk>
2024-11-06 18:56:36 +0000

    feat: use sscache for building

--- home/retoran/.env/terminal/compilers.zsh
+++ home/retoran/.env/terminal/compilers.zsh
@@ -2,3 +2,5 @@
 export CMAKE_BUILD_PARALLEL_LEVEL=24
 # use cache for building with rust
 export RUSTC_WRAPPER=sccache
+export DCMAKE_C_COMPILER_LAUNCHER=sccache
+export DCMAKE_CXX_COMPILER_LAUNCHER=sccache
