bb724db87004aaf905c26505ae2ad612410c9428
RetoranPetra <flyro@live.co.uk>
2025-05-03 14:36:57 +0100

    feat: only set gem home when gem is present on system

--- home/retoran/.env/terminal/ruby.zsh
+++ home/retoran/.env/terminal/ruby.zsh
@@ -1,3 +1,5 @@
 #!/bin/zsh
-export GEM_HOME="$(gem env user_gemhome)"
-export PATH="$PATH:$GEM_HOME/bin"
+if where gem > /dev/null; then
+	export GEM_HOME="$(gem env user_gemhome)"
+	export PATH="$PATH:$GEM_HOME/bin"
+fi
