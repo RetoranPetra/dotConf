8398c9f0b47f04a867b03e482d8d3aa1f8f7b794
RetoranPetra <flyro@live.co.uk>
2025-07-29 21:17:28 +0100

    fix: preserve date metadata when converting gifs

--- home/retoran/.config/gallery-dl/giftomp4.bash
+++ home/retoran/.config/gallery-dl/giftomp4.bash
@@ -1,2 +1,9 @@
 #!/bin/bash
-ffmpeg -i "$1" -pix_fmt yuv420p -vf 'scale=trunc(iw/2)*2:trunc(ih/2)*2' "${1%.gif}.mp4" -y && rm "$1"
+newFile="${1%.gif}.mp4"
+ffmpeg -i "$1" -pix_fmt yuv420p -vf 'scale=trunc(iw/2)*2:trunc(ih/2)*2' "$newFile" -y
+err=$?
+if [ $err -ne 0 ]; then
+	exit $err
+fi
+touch -r "$1" "$newFile" && rm "$1"
+exit $?
