25b89d9cb4cfdf5c920d935c184379453ad5be09
RetoranPetra <flyro@live.co.uk>
2025-09-26 23:03:17 +0100

    attempt at fcitx5

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -442,14 +442,33 @@
 
     i18n.inputMethod = {
       type = "fcitx5";
+      package = pkgs.fcitx5;
       fcitx5 = {
         addons = with pkgs; [ fcitx5-mozc ];
         settings = {
+          globalOptions = {
+            Hotkey = {
+              EnumerateWithTriggerKeys = false;
+              ModifierOnlyKeyTimeout = 250;
+              TriggerKeys = {
+                "0" = "Control+Alt+space";
+                "1" = "Zenkaku_Hankaku";
+                "2" = "Hangul";
+              };
+              ActivateKeys = {
+                "0" = "Control+grave";
+              };
+              DeactivateKeys = {
+                "0" = "Control-notsign";
+              };
+            };
+          };
           inputMethod = {
             GroupOrder."0" = "Default";
             "Groups/0" = {
               Name = "Default";
               "Default Layout" = "gb";
+              DefaultIM = "mozc";
             };
             "Groups/0/Items/0".Name = "keyboard-gb";
             "Groups/0/Items/1".Name = "mozc";
@@ -472,8 +491,6 @@
       hyprcursor
       qt5.qtwayland
       qt6.qtwayland
-      #Need to check how this gets configured properly.
-      fcitx5
 
       # System utilities
       qdirstat
