c29771c7011fb305bec16fb4b9491909c9502c18
RetoranPetra <flyro@live.co.uk>
2025-09-28 19:56:12 +0100

    swap to nixvim

--- etc/nixos/configuration.d/home.retoran/neovim.nix
+++ etc/nixos/configuration.d/home.retoran/neovim.nix
@@ -1,35 +1,30 @@
+let
+	nixvim = import (builtins.fetchGit {
+		url = "https://github.com/nix-community/nixvim";
+		ref = "nixos-25.05";
+	});
+in
 { config, lib, pkgs, ... }:
 {
-	imports = [ <home-manager/nixos> ];
-	programs.neovim = {
-		enable = true;
-		defaultEditor = true;
-	};
+	imports =
+		[
+			<home-manager/nixos>
+		];
 	home-manager.users.retoran = { pkgs, ... }: {
+		imports = [
+			nixvim.homeModules.nixvim
+		];
 		# Relies on git
 		programs.git.enable = true;
-		programs.git.extraConfig.core.editor = "nvim";
+		#programs.git.extraConfig.core.editor = "nvim";
 		# Relies on lazygit
 		programs.lazygit.enable = true;
 
-		# Configuration
-    xdg.configFile."nvim".source = /etc/nixos/home.retoran/home/retoran/.config/nvim;
-
-		# Dependencies
-		home.packages = with pkgs; [
-			python3
-			nodejs
-			llvmPackages_latest.clang
-			llvmPackages_latest.libcxx
-			llvmPackages_latest.libllvm
-			llvmPackages_latest.lldb
-			unzip
-			go
-			cmake
-			gnumake
-			fzf
-			lazygit
-			ripgrep
-		];
+		programs.nixvim = {
+			imports = [
+				/etc/nixos/home.retoran/home/retoran/.config/nvim/config
+			];
+			enable = true;
+		};
 	};
 }
