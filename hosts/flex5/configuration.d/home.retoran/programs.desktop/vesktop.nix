d65ba9abb5b2b94bfa0f52d6b8dbc438b2120442
RetoranPetra <flyro@live.co.uk>
2025-09-30 20:03:45 +0100

    correct vesktop path

--- hosts/flex5/configuration.d/home.retoran/programs.desktop/vesktop.nix
+++ hosts/flex5/configuration.d/home.retoran/programs.desktop/vesktop.nix
@@ -2,7 +2,7 @@
 {
   # Vesktop has a programs.vesktop implementation. Should use that instead.
   xdg.configFile."vesktop" = {
-    source = ./../../../../../root/home/.config/vesktop;
+    source = ./../../../../../root/home/retoran/.config/vesktop;
     recursive = true;
   };
   home.packages = with pkgs; [
