2729d26b8138a952304137a2db7ffa52c27d796f
RetoranPetra <flyro@live.co.uk>
2025-10-16 23:12:11 +0100

    fix: set default editor for user as nvim

--- hosts/flex5/configuration.d/home.retoran/neovim.nix
+++ hosts/flex5/configuration.d/home.retoran/neovim.nix
@@ -5,6 +5,9 @@
   #programs.git.extraConfig.core.editor = "nvim";
   # Relies on lazygit
   programs.lazygit.enable = true;
+  home.sessionVariables = {
+    "EDITOR" = "nvim";
+  };
 
   programs.nixvim = {
     imports = [ ./../../../../modules/nvim/config ];
