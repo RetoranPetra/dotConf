68f14515c68ef970652fcc55c67c2d6ef1d7ab92
RetoranPetra <flyro@live.co.uk>
2025-11-09 09:57:19 +0000

    remove vesktop config file

--- hosts/flex5/configuration.d/home.retoran/programs.desktop/vesktop/default.nix
+++ hosts/flex5/configuration.d/home.retoran/programs.desktop/vesktop/default.nix
@@ -1,9 +1,5 @@
 { pkgs, ... }: {
   # Vesktop has a programs.vesktop implementation. Should use that instead.
-  xdg.configFile."vesktop" = {
-    source = ./config;
-    recursive = true;
-  };
   home.packages = with pkgs; [
     vesktop
   ];
