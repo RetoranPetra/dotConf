9f3e57efd19931310e8a3ff935340efe51efdcba
RetoranPetra <flyro@live.co.uk>
2025-12-22 19:52:08 +0000

    feat: add vesktop plugins

--- hosts/flex5/configuration.d/home.retoran/programs.desktop/vesktop/default.nix
+++ hosts/flex5/configuration.d/home.retoran/programs.desktop/vesktop/default.nix
@@ -18,7 +18,38 @@
           "HorizontalServerList.css"
           "SkeuoCord.css"
         ];
-        plugins = [];
+        plugins = {
+          BetterGifPicker.enabled = true;
+          "WebRichPresence (arRPC)".enabled = true;
+          MessageLogger = {
+            enabled = true;
+
+            collapseDeleted = false;
+
+            deleteStyle = "text";
+
+            ignoreBots = false;
+            ignoreSelf = false;
+
+            logEdits = true;
+            logDeletes = true;
+            inlineEdits = true;
+          };
+
+          TypingIndicator = {
+            enabled = true;
+            includeCurrentChannel = true;
+            indicatorMode = 3;
+          };
+          TypingTweaks = {
+            enabled = true;
+            alternativeFormatting = true;
+            showRoleColors = true;
+            showAvatars = true;
+          };
+
+          # TODO: Add custom vesktop plugins.
+        };
       };
       # Should set themes in this list AND enabledthemes with same list, it's dumb there are two lists.
       themes = {
