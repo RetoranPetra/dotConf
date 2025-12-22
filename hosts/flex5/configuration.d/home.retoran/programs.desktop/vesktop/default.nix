039dc7a11ab22508a0654947f4b017b45428f1f8
RetoranPetra <flyro@live.co.uk>
2025-12-22 19:11:22 +0000

    refactor: use programs.vesktop

--- hosts/flex5/configuration.d/home.retoran/programs.desktop/vesktop/default.nix
+++ hosts/flex5/configuration.d/home.retoran/programs.desktop/vesktop/default.nix
@@ -1,6 +1,31 @@
 { pkgs, ... }: {
-  # Vesktop has a programs.vesktop implementation. Should use that instead.
-  home.packages = with pkgs; [
-    vesktop
-  ];
+  programs.vesktop = {
+    settings = {
+      discordBranch = "stable";
+      minimizeToTray = true;
+      arRPC = true;
+      spellCheckLanguages = [
+        "en-GB"
+        "en"
+      ];
+      hardwareAcceleration = true;
+    };
+    vencord = {
+      settings = {
+        autoUpdate = true;
+        autoUpdateNotification = true;
+        enabledThemes = [
+          "HorizontalServerList.css"
+          "SkeuoCord.css"
+        ];
+        plugins = [];
+      };
+      # Should set themes in this list AND enabledthemes with same list, it's dumb there are two lists.
+      themes = {
+        "HorizontalServerList" = ./config/themes/HorizontalServerList.theme.css;
+        "SkeuoCord" = ./config/themes/SkeuoCord.theme.css;
+      };
+    };
+    enable = true;
+  };
 }
