aafc282c7a3bc37ecbe25204947557dd3fb200c5
RetoranPetra <flyro@live.co.uk>
2026-04-12 20:37:15 +0100

    feat: allow enrolling of main drive encryption keys to TPM2 module

--- hosts/desktop/nixos/boot.nix
+++ hosts/desktop/nixos/boot.nix
@@ -1,6 +1,20 @@
-{ pkgs, lib, ...}: {
+{ pkgs, lib, ...}:
+let
+  # This just adds a script to our bin to easily enroll TPM2 to our root.
+  luksEnrollTPM = pkgs.writeTextFile {
+    name = "luksEnrollTPM";
+    destination = "/bin/luksEnrollTPM";
+    executable = true;
+    text = "systemd-cryptenroll --wipe-slot=tpm2 --tpm2-device=auto --tpm2-pcrs=0+7 /dev/nvme1n1p2";
+  };
+in
+# Enroll TPM2 keys with systemd-cryptenroll --wipe-slot=tpm2 --tpm2-device=auto --tpm2-pcrs=0+7 /dev/disk/by-uuid/2611-2EA3
+{
   environment.systemPackages = [
+    luksEnrollTPM
     pkgs.sbctl
+    # Needed for TPM2 key enrollment with systemd-cryptenroll
+    pkgs.tpm2-tss
   ];
 
   # Forcefully disable systemd-boot in favour of lanzaboote
