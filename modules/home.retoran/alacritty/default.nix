477f1bed86355d98446881290f76c34e3036e628
RetoranPetra <flyro@live.co.uk>
2026-04-14 13:34:10 +0100

    fix: alacritty is the preferred terminal emulator in xdg

--- modules/home.retoran/alacritty/default.nix
+++ modules/home.retoran/alacritty/default.nix
@@ -6,4 +6,10 @@
     "x-xcheme-handler/terminal" = [ "alacritty.desktop" ];
   };
   xdg.configFile."xfce4/helpers.rc".text = "TerminalEmulator=alacritty";
+
+  # A lot of programs respect this surprisingly.
+  xdg.terminal-exec = {
+    enable = true;
+    settings.default = [ "alacritty.desktop" ];
+  };
 }
