8b48ab1acc31462e8df36d16b7c3a9bce93af954
RetoranPetra <flyro@live.co.uk>
2026-04-16 19:43:56 +0100

    feat: use resolved for networking

--- modules/nixos/configuration.d/systemd-network.nix
+++ modules/nixos/configuration.d/systemd-network.nix
@@ -43,4 +43,29 @@
   # Firewall
   networking.nftables.enable = true;
   networking.firewall.enable = true;
+
+  services.resolved = {
+    enable = true;
+    settings.Resolve = {
+      Domains = ["~."];
+      FallbackDns = [
+        "9.9.9.9#dns.quad9.net"
+        "8.8.8.8#dns.google"
+        "2606:4700:4700::1111#cloudflare-dns.com"
+        "2620:fe::9#dns.quad9.net"
+        "2001:4860:4860::8888#dns.google"];
+      # Defaults
+      DNSSEC = true;
+      Cache = true;
+      CacheFromLocalHost = false;
+      DNSStubListener = true;
+      ReadEtcHosts = true;
+      DNSOverTLS = false;
+      LLMNR = true;
+    };
+  };
+
+  networking.nameservers = [
+    "1.1.1.1#cloudflare-dns.com"
+  ];
 }
