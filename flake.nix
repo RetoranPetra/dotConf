a31f6672c62668dc9e7e63ea3eec5c5fadb52b38
RetoranPetra <flyro@live.co.uk>
2026-04-20 23:46:19 +0100

    fix: remove unneeded follow in flake-parts

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
