e50a568972322270f68eb4cb53650db253d56c09
RetoranPetra <flyro@live.co.uk>
2026-04-26 16:32:29 +0100

    feat: manual vintagestory package update

--- flake.nix
+++ flake.nix
@@ -172,6 +172,48 @@
                       stdenvNoCC = prev.stdenvNoCC;
                       fetchzip = prev.fetchzip;
                     };
+                    vintagestory =
+                      let
+                        version = "1.22.0";
+                      in
+                      prev.vintagestory.overrideAttrs {
+                        version = version;
+                        src = prev.fetchurl {
+                          url = "https://cdn.vintagestory.at/gamefiles/stable/vs_client_linux-x64_${version}.tar.gz";
+                          hash = "sha256-c90Mb5hyL8StLFrKokAgER/u6l3jhhluP5ErgVs4geI=";
+                        };
+                        installPhase = ''
+                          runHook preInstall
+
+                          mkdir -p $out/share/vintagestory $out/bin $out/share/icons/hicolor/512x512/apps $out/share/fonts/truetype
+                          cp -r * $out/share/vintagestory
+
+                          # Old way
+                          #magick $out/share/vintagestory/assets/gameicon.xpm $out/share/icons/hicolor/512x512/apps/vintagestory.png
+                          cp $out/share/vintagestory/assets/gameicon.png $out/share/icons/hicolor/512x512/apps/vintagestory.png
+                          cp $out/share/vintagestory/assets/game/fonts/*.ttf $out/share/fonts/truetype
+
+                          rm -rvf $out/share/vintagestory/{install,run,server}.sh
+
+                          runHook postInstall
+                        '';
+                        preFixup = ''
+                           makeWrapperArgs+=(--prefix LD_LIBRARY_PATH : "$runtimeLibraryPath")
+
+                           makeWrapper ${prev.lib.meta.getExe prev.dotnet-runtime_10} $out/bin/vintagestory \
+                            "''${makeWrapperArgs[@]}" \
+                             --add-flags $out/share/vintagestory/Vintagestory.dll
+
+                          makeWrapper ${prev.lib.getExe prev.dotnet-runtime_10} $out/bin/vintagestory-server \
+                            "''${makeWrapperArgs[@]}" \
+                            --add-flags $out/share/vintagestory/VintagestoryServer.dll
+
+                           find "$out/share/vintagestory/assets/" -not -path "*/fonts/*" -regex ".*/.*[A-Z].*" | while read -r file; do
+                             local filename="$(basename -- "$file")"
+                             ln -sf "$filename" "''${file%/*}"/"''${filename,,}"
+                           done
+                        '';
+                      };
                   })
                 ];
                 config = config // {
