bb557b44ae4e39bde1832abd4ed7aa4304ccbb5b
RetoranPetra <flyro@live.co.uk>
2026-05-19 13:59:19 +0100

    Revert "fix: override linux kernel version until there's a fix for bluetooth adaptor"
    This reverts commit 0d46514e725c20633eb260a0a028cbd875717ab4.

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -9,23 +9,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # NOTE: Override version 7.0.7 with 7.0.6 until there's a fix in 7.0.8/7.0.9 for our bluetooth issues.
-  boot.kernelPackages =
-    let
-      # 7.0.6 only has zen1
-      version = "7.0.6";
-      suffix = "zen1";
-    in
-    pkgs.linuxPackagesFor (
-      pkgs.linuxPackages_zen.kernel.override {
-        inherit version suffix;
-        modDirVersion = lib.versions.pad 3 "${version}-${suffix}";
-        src = pkgs.fetchFromGitHub {
-          owner = "zen-kernel";
-          repo = "zen-kernel";
-          rev = "v${version}-${suffix}";
-          sha256 = "0b27mh7rndbyjbqf4gfivacgda9srkjlh1hdflasl0vq4sdy3d68";
-        };
-      }
-    );
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 }
