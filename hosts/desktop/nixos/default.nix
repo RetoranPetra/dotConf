1869a47872ddcc237f868f970a05f3c8f4159c5d
RetoranPetra <flyro@live.co.uk>
2026-06-02 19:01:13 +0100

    feat: add rootless docker

--- hosts/desktop/nixos/default.nix
+++ hosts/desktop/nixos/default.nix
@@ -3,6 +3,7 @@
   imports = [
     ./../../../modules/nixos
     ./../../../modules/nixos/configuration.d/obs-studio-virtualCamera.nix
+    ./../../../modules/nixos/configuration.d/docker.nix
     ./hardware-configuration.nix
     ./boot.nix
   ];
