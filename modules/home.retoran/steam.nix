d4d07db54e02db77b9b84130cf860245e0344e76
RetoranPetra <flyro@live.co.uk>
2026-08-11 20:23:19 +0100

    comment on what might need doing

--- modules/home.retoran/steam.nix
+++ modules/home.retoran/steam.nix
@@ -9,20 +9,16 @@ in
 {
   home.file = (
     builtins.listToAttrs (
-      map
-        (compatTool: {
-          name = ".local/share/Steam/compatibilitytools.d/${compatTool.pname}";
-          value = {
-            source = compatTool.steamcompattool;
-          };
-        })
-        [
-          pkgs.steamtinkerlaunch
-          pkgs.proton-ge-bin
-          pkgs.proton-dw-bin
-        ]
+      map (compatTool: {
+        name = ".local/share/Steam/compatibilitytools.d/${compatTool.pname}";
+        value = {
+          source = compatTool.steamcompattool;
+        };
+      }) compatTools
     )
   );
+  # Think I need to run "steamtinkerlaunch p list" after updating the compat tools for everything to behave.
+
   # Add steamtinkerlaunch to path
-  home.packages = [pkgs.steamtinkerlaunch];
+  home.packages = [ pkgs.steamtinkerlaunch ];
 }
